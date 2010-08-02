5d71bf79415a1bf5f769ded1af1b940527d12f4c
Robb Broome <robb.broome@playlist.com>
2010-08-02 13:40:44 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Catkazoo::Application.config.secret_token = '1a6829bf6a71a595685631da89de7745d074571ec99165298e0eae419db4b421904baf047c913c443c60690bb8598a09e5d5f1443993ccf6177ca96c2430efd3'
